be946a37c68289af60a88e96d1ea3ac3a6e2bfdf
Tracy Ramirez <tracy.ramirez92@gmail.com>
2025-04-30 14:48:07 -0600

    corregir algunos

--- dist/altralabsweb/server/main.server.mjs
+++ dist/altralabsweb/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-JDBZ4RXF.mjs";import"./chunk-XJF56TRR.mjs";import"./chunk-K4GGJEOI.mjs";import"./chunk-RCNBPQVM.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-4GUPOS47.mjs";import"./chunk-XJF56TRR.mjs";import"./chunk-K4GGJEOI.mjs";import"./chunk-RCNBPQVM.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
